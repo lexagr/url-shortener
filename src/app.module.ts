b3b7f798603de5d0befecf808c91c63c009a0f81
Alexey <lexa.grydnistiy123@gmail.com>
2021-07-30 10:01:35 +0300

    Config moved to ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123',
-      database: 'shortener',
-      synchronize: false,
-    }),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
